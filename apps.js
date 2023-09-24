7a7aaa2e5e6295bd8adc0234de20eb5d9044471a
Muhammad Hamza <143834218+hhamza56@users.noreply.github.com>
2023-09-24 22:19:21 +0100

    wrok complete

---
+++ apps.js
@@ -0,0 +1,207 @@
+
+// var name1 = prompt("enter your name ")
+//   var lover = userinput.tolowercase();
+
+// for(var i =0; i<name1.length;i++){
+//     console.log(name1===userinput)
+// } 
+
+
+
+// var userinput = prompt("Enter Your Name")
+// var lower = user.touppercase()
+// for(i =0; i<userinput; i++){
+//     alert("not natch")
+
+// }
+
+// var userinput = prompt("enter your name");
+// var  arry = ["karachi","lahore","panjab",];
+// var matchfound = false;
+// for(i =0; i <arry.length; i++)
+
+// if(userinput===arry[i]){
+//   var found = true
+//   console.log("welcome"+ " " + userinput)
+//   break;
+
+// }if(matchfound+found===false) {
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var userinput = prompt("enter your name");
+// var firstname = ["hamza, hussain, hashir"];
+// var lastname = ["hashir, hussain, hamza"];
+// var fullname=[""];
+// for (i = 0; i < firstname.length; i++){
+// for(k = 0; k < lastname.length; k++){
+//   console.log ( firstname[i] + " " + lastname[k])
+// }  
+// }
+
+
+// var city = prompt("Enter Your City Name");
+// city=city.toUpperCase();
+// var arr=["KARACHI","HYDERABAD","LAHORE","PANJAB","ISLAMABAD"];
+//  var  flage = false;
+
+// for(var i = 0; i <arr.length; i++){
+  
+//   if(city===arr[i]){
+//   flage = true;
+//   }
+// }
+// if(flage){
+//   console.log(city)
+//   }else{
+//     console.log("not match")
+//   }
+
+
+
+// var city = prompt("Enter Your City Name");
+// city=city.toLowerCase();
+// var arr=["karachi","hyderabad","lahore","panjab","islamabad"];
+//  var  flage = false;
+
+// for(var i = 0; i <arr.length; i++){
+  
+//   if(city===arr[i]){
+//   flage = true;
+//   }
+// }
+// if(flage){
+//   console.log(city)
+//   }else{
+//     console.log("not match")
+//   }
+
+
+
+
+// var arry = [ "hamza","hussain","hashir","sami","ubaid"]
+
+// var firstchar = arry.slice(0,2);
+// var otherchar= arry.slice(1);
+// firstchar = firstchar.touppercase();
+// otherchar = otherchar.tolowercase();
+// var hamza = firstchar+otherchar;
+// console.log(arry)
+
+
+
+
+
+
+// var str = prompt("Enter some text");
+//  var numChars = str.length;
+//  for (var i = 0; i < numChars; i++) {
+//  if (str.slice(i, i + 2) === "  ") {
+//  alert("No double spaces!");
+//  break;
+
+//  }
+//  }
+
+
+//  var month = prompt("Enter a month");
+// var charsInMonth = month.length;
+//  if (charsInMonth > 3) {
+//  monthAbbrev = month.slice(0, 3);
+//  }
+
+
+
+
+// var username ="The code loops through the string looking for World War II Line 2 progresses the string character by character examining each character sequence If it finds WorldWar  line concatenates three segments all the characters preceding World War  thesubstitute phrase the Second World Warand then all the characters following World"
+
+// for( var i = 0; i  <username.length; i++){
+// if (username.slice(i, i + 24) === "string looking for World"){
+//   username= username.slice(0 ,i) +  "hazma hussain hashir "  + username.slice(i + 24)
+// }
+// }console.log(username)
+
+
+// var firstchar = text.indexof ("welcome to hall")
+// if(firstchar !== -1){
+//     text = text.slice(0,firstchar) + "hamza hussain" + text.slice(firstchar + 15)
+//         console.log(firstchar)
+    
+// }
+
+
+// var newtext = "hello i hamza hussain im profashional web developer"
+// newtext = newtext.replace(/hamza/, "hashir");{
+//     console.log(newtext)
+// }
+
+
+
+
+// var num = 2.5;
+// console.log(Math.round(num))
+
+
+// var num1 = 225;
+// console.log(Math.ceil(num1))
+
+
+// var num2 = 78.25
+// console.log(Math.floor(num2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ newFile.js
@@ -0,0 +1,7 @@
+// var lower = userinput.toUpperCase();
+for (i = 0; i < arry.length; i++) {
+  if (userinput === arry) {
+    console.log("welcome");
+  }
+
+}
